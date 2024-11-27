43b8318030341c0c3d3ccf0447668fa729a4b9eb
Madara909 <sashankbhardwaj99@gmail.com>
2024-11-28 00:10:54 +0530

    complete project

---
+++ lib/validation.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+    title: z.string().min(3).max(100),
+    description: z.string().min(20).max(500),
+    category: z.string().min(3).max(20),
+    link:z
+  .string()
+  .url()
+  .refine(
+    async (url) => {
+      try {
+        const res = await fetch(url, { method: "HEAD" });
+
+        if (!res.ok) {
+          // If the response is not OK, the URL is not valid
+          return false;
+        }
+
+        const contentType = res.headers.get("content-type");
+        // Check if the content type indicates an image
+        return contentType?.startsWith("image/");
+      } catch {
+        // Return false if any error occurs during the fetch
+        return false;
+      }
+    },
+    {
+      message: "The provided URL is not a valid image link.",
+    }
+  ),
+    pitch: z.string().min(10),
+});
